bd867b1083e04b484217dd711905a8e3f6751dfe
Almamun Hossain <almamunhossains@gmail.com>
2023-07-10 11:08:51 +0600

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
-  app.enableCors();
+  app.enableCors({
+    origin: ["https://ebazar-three.vercel.app", 'http:://localhost:3000']
+  });
+
   await app.listen(3002);
 }
 bootstrap();
